9a3c4abfb1ef492d85f620c65fdc52211d54eaad
Dck Token <dicktoken@gmail.com>
2016-09-28 14:18:24 +0200

    Make $LINE to execute itself first time is called

--- unpack.bash
+++ unpack.bash
@@ -10,7 +10,7 @@ OUTPUT="$(find . -mindepth 1 -type d | wc -l)" #how many doujinshi at folder
 echo -e $"\n~$ARG|${OUTPUT}" >> $FILE #	makes the show entrance
 for (( i = 1; i < $OUTPUT + 1; i++ )); do #for every doujinshi do
 	LINE="$(ls -1 | sed -n "$i p" )" # the line is the $ith line
-	cd "$LINE" #enters into it
+	cd "${LINE}" #enters into it
 	rm _* #deletes unnecesary bullshit
 	FILENAME="$(ls -1tr | grep -e '-1.jpg' | cut -d '1' -f 1)" #gets the filename, its safe to use cut because i alredy grep'ed the first file
 	PAGES="$(ls -1 | wc -l)" # pages the linecount of ls -1
