98879467fb20bba28d828d193b5bf2e6a39b7f70
Dck Token <dicktoken@gmail.com>
2016-10-01 16:49:26 +0200

    Reduced verbose level

--- run.py
+++ run.py
@@ -13,45 +13,34 @@ async def search(ctx, *, show_name : str):
     """Searches for a show and lists all related doujinshi."""
     arg = show_name
     query = "~" + arg + "|"
-    print('Query: ' + query) #Debug
     with open('list.txt') as f:
         for num, line in enumerate(f, 1):
             if query.lower() in line.lower():
-                #await bot.send_message(message.channel, 'Found at line ' + str(num)) #Debug
                 error = False
                 break
             else:
                 error = True
-        print(error) #Debug
         #There must be a better way than a bool check, but fuck it
         if error is True:
             await bot.say('Not found! Try an exact show name or use ``$listshows``')
         elif error is False:
-            print(num) #Debug
-            print(line) #Debug
             show = arg
             files = int(line.rstrip('\n').split('|')[1])
-            #await bot.send_message(message.channel, 'Number of files: ' + str(files)) #Debug
             f=open('list.txt')
             lines=f.readlines()
             i = num
             choice = 1
             p = 1
-            await bot.say('Show found, check your DMs!')
             await bot.send_message(ctx.message.author, 'Found ``' + str(files) + '`` doujinshi')
             await bot.send_typing(ctx.message.author)
             pages = (int(files / 11)) + 1
-            await bot.send_message(ctx.message.author, '[DEBUG] Pages: ' + str(pages))
             if pages == 1:
-                print('inside 1 page condition')
                 while i < num + files:
-                    print(i)
                     currentline = lines[i]
                     name = currentline.rstrip('\n').split(':')[0]
                     doujinshifiles = currentline.rstrip('\n').split(':')[2]
                     await bot.send_message(ctx.message.author, '``' +str(choice) + '``) ' + name + ' (``' + doujinshifiles + '`` pages)')
                     await bot.send_typing(ctx.message.author)
-                    #await bot.send_message(message.channel, currentline) #Debug
                     if choice == files:
                         await bot.send_message(ctx.message.author, '**Last page reached.**\nType a selection to continue or type ``exit`` to cancel')
                         pick = await bot.wait_for_message(timeout=20.0, author=ctx.message.author)
@@ -68,27 +57,20 @@ async def search(ctx, *, show_name : str):
                     i = i + 1
                     choice = choice + 1
             else: #when there is more than one page
-                print('inside +1 page condition!')
                 choice = 1
                 currentpage = 1
                 while currentpage <= pages:
-                    print(currentpage)
                     while choice < currentpage * 10 + 1:
                         await bot.send_message(ctx.message.author, 'Page number ``' + str(currentpage) + '``/``' + str(pages) + '``:')
                         while i < num + files: #for kill la kill is "i < 68 + 13" (which is 81, last line) so this is :ok_hand:
-                            print('i: ' + str(i))
-                            print('choice: ' + str(choice))
-                            print('files; ' + str(files))
                             currentline = lines[i] #starts at index 0, be careful!
                             name = currentline.rstrip('\n').split(':')[0]
                             doujinshifiles = currentline.rstrip('\n').split(':')[2]
                             await bot.send_message(ctx.message.author, '``' +str(choice) + '``) ' + name + ' (``' + doujinshifiles + '`` pages)')
                             await bot.send_typing(ctx.message.author)
-                            #await bot.send_message(message.channel, currentline) #Debug
                             choice = choice + 1
                             i = i + 1
                             pagesleft = pages - currentpage
-                            print('currentpage: ' + str(currentpage * 10 + 1))
                             if choice > currentpage * 10:
                                 await bot.send_message(ctx.message.author, '**Page end reached** (``' + str(pagesleft) + '`` pages left).\nType a selection to continue.\nIf you want to countinue type ``next``. If you want to stop, type ``exit`` instead!')
                                 pick = await bot.wait_for_message(timeout=20.0, author=ctx.message.author)
@@ -122,6 +104,7 @@ async def search(ctx, *, show_name : str):
 @bot.command(pass_context=True)
 async def listshows(ctx):
     """Lists every show in the database."""
+    print('User ' + str(ctx.message.author) + ' used $listshows on channel "' + str(ctx.message.channel) +'"')
     await bot.say('Shows in my database:')
     i = 0
     with open('list.txt') as f:
@@ -129,11 +112,9 @@ async def listshows(ctx):
         num_lines = file_len('list.txt')
         l = 0
         i = 1
-        print(num_lines)
         while l < num_lines + 1:
             await bot.send_typing(ctx.message.channel)
             currentline = lines[l]
-            print(currentline)
             name = currentline.rstrip('\n').split('|')[0]
             name = name.split('~')[1]
             files = currentline.rstrip('\n').split('|')[1]
@@ -145,9 +126,6 @@ async def listshows(ctx):
             except ValueError:
                 howdoicallthis = 0
             pages = (int(i / 11)) + 1
-            print('i is: ' + str(i))
-            print('pages is: ' + str(pages))
-            print('howdoicallthis is: ' + str(howdoicallthis))
             if howdoicallthis > pages:
                 await bot.say('**Page end reached**.\nIf you want to countinue type ``next``. If you want to stop, type ``exit`` instead!')
                 pick = await bot.wait_for_message(timeout=20.0, author=(ctx.message.author))
@@ -161,9 +139,6 @@ async def listshows(ctx):
                     continue
             else:
                 pass
-    #await bot.send_message(message.channel, '[DEBUG] howdoicallthis value: ' + str(howdoicallthis))
-    #await bot.send_message(message.channel, '[DEBUG] pages value: ' + str (pages))
-    #await bot.send_message(message.channel, '[DEBUG] i value: ' + str(i))
     await bot.say('If you want a show added, use ``$suggest <show name>``')
 
 @bot.command(pass_context=True)
@@ -181,7 +156,6 @@ async def suggest(ctx, *, show_name: str):
 @bot.command(pass_context=True)
 async def exact(ctx, show_name : str, name : str):
     """Dumps a doujinshi by providing the show and the exact name."""
-    #await bot.send_message(message.channel, 'Show name: ``' + show + '``\nDoujinshi name: ``' + name + '``') #Debug
     showquery = '~' + show_name + '|'
     with open('list.txt') as f:
         lines=f.readlines()
@@ -189,7 +163,6 @@ async def exact(ctx, show_name : str, name : str):
         l = 0
         while l < num_lines:
             currentline = lines[l]
-            #print(currentline) #Debug
             files = int(currentline.split('|')[1])
             await bot.send_typing(ctx.message.channel)
             if showquery.lower() in currentline.lower():
@@ -206,18 +179,14 @@ async def exact(ctx, show_name : str, name : str):
         if error == False:
             l = l + 1
             showline = l
-            await bot.say('Show found!')
-            #await bot.send_message(message.channel, 'Current line: ``' + currentline + '``') #Debug
-            #await bot.send_message(message.channel, '[DEBUG] Max: ' + str(showline + files - 1))
+            await bot.say('Show found! Searching for the doujinshi...')
             while l <= showline + files - 1:
                 currentline = lines[l]
                 await bot.send_typing(ctx.message.channel)
                 if name.lower() in currentline.lower():
-                    await bot.say('Doujinshi found too!')
                     await dump_doujinshi(l, ctx.message, lines, showline)
                     return
                 else:
-                    #await bot.send_message(message.channel, '[DEBUG] Not found, current line is ``' + currentline + '`` and l is ``' + str(l) + '``')
                     l = l + 1
                     error = True
             if error == True:
@@ -238,44 +207,30 @@ async def dump_doujinshi(doujinshiline, message, lines, num):
     pages = int(currentline.rstrip('\n').split(':')[2])
     extension = currentline.rstrip('\n').split(':')[3]
     currentline = lines[num - 1]
-    print(currentline)
     show = currentline.split('~')[1]
     show = show.split('|')[0]
-    #await bot.send_message(message.author, '[DEBUG] Line number: ' + str(num))
-    #await bot.send_message(message.author, '[DEBUG] Full line: ' + line)
-    #await bot.send_message(message.author, 'Show: ``' + show + '``')
-    #await bot.send_message(message.author, 'Doujinshi name: ``' + name + '``')
-    #await bot.send_message(message.author, 'Pages: ``' + str(pages) + '``')
     await bot.send_message(message.author, '''Show: ``{}``\nDoujinshi name: ``{}``\nPages: ``{}``'''.format(show, name, str(pages)))
     i = 1
     while i < pages + 1:
         path = "{}/{}/{}{}.{}".format(show, name, filename, str(i), extension)
-        #await asyncio.sleep(1) #lib handles ratelimits
-        #await bot.send_message(message.author, 'Page number ``' + str(i) + '``')
-        #await bot.send_file(message.author, show + '/' + name + '/' + filename + str(i) + '.' + extension)
         content = "Page ``{}``/``{}``".format(str(i), str(pages))
         await bot.send_file(message.author, path, content=content)
-        print('Sent file ' + str(i))
+        print('Sent file ' + str(i) + ' of doujinshi "' + name + '" to user ' + str(message.author))
         i = i + 1
     await bot.send_message(message.author, 'Dump finished! :ok_hand:')
-    print('Dump finished!')
+    print('Dump finished for user ' + str(message.author))
 
             
 @bot.event
 async def on_ready():
-    print('Logged in as')
-    print(bot.user.name + ' (id: ' + bot.user.id + ')')
+    print('Logged in as ' + bot.user.name + ' (id: ' + bot.user.id + ')')
     print('------')
-    #print('Opening list.txt')
-    #f = open('list.txt', 'r')
-    #print('Everything done!')
-    #print('------')
     await bot.change_status(discord.Game(name='with doujinshi!'))
 
 @bot.event
 async def on_command_error(err, ctx):
     print('Error ' + str(err) + ' catched!')
-    await bot.send_message(ctx.message.channel, 'Error! ``' + str(err) + '``\nContact @TheGooDFeelinG#4615 for help!')
+    await bot.send_message(ctx.message.channel, 'Error! ``' + str(err) + '``\nContact TheGooDFeelinG#4615 for help!')
 
 token = os.getenv('TOKEN')
 bot.run(token)
